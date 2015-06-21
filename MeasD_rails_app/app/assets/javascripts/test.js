6fbf31d02867df0c12102765b4dbd32ecc919224
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2015-06-21 14:21:14 -0700

    save work

--- MeasD_rails_app/app/assets/javascripts/test.js
+++ MeasD_rails_app/app/assets/javascripts/test.js
@@ -1,6 +1,9 @@
 $(document).on("ready",function(){
-    var draw = SVG('test').size(300, 300);
+    var draw = SVG('test').size(500, 500);
+    var group = draw.group()
     var rect = draw.rect(100, 100).attr({ fill: '#f06' });
+    var rect2 = draw.rect(50, 50).attr({ fill: '#f86' });
+
     rect.draggable();
 })
 
