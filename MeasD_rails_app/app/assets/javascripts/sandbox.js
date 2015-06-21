5a2c4213c592bafa1b7d7a7b495c7c73d36e588e
Majd Murad <murad.majd@gmail.com>
2015-06-20 23:35:32 -0700

    Grouped toolbox furniture, sseparate cloning

--- MeasD_rails_app/app/assets/javascripts/sandbox.js
+++ MeasD_rails_app/app/assets/javascripts/sandbox.js
@@ -1,37 +1,47 @@
 
 $(document).ready(function(){
-  var draw = SVG('drawing').size(800, 400)
-//   var rect = draw.rect(100, 100).fill('#f06')
-//   rect.draggable({
-//   minX: 10
-// , minY: 15
-// , maxX: 200
-// , maxY: 300
-// });
+  var draw = SVG('drawing').size(800, 600)
+
   var roomWidth = 400;
   var roomLength = 250;
-  var line = draw.line(0,350, 800,350).stroke({ width: 1 })
+  var line = draw.line(0,500, 800,500).stroke({ width: 1 })
 
   var room = draw.rect(roomWidth,roomLength)
   room.fill('white')
   room.stroke({color: 'black', width: 1})
   room.move(200,50)
 
-  var bed = draw.image('assets/bed.png', 75,50).move(50,350)
-
-  bed.on('click', function(){
-    console.log('bed clicked')
+  $('#room_form').submit(function(e){
+    e.preventDefault();
+    room.width($('#room_width').val())
+    room.height($('#room_length').val())
   })
 
-  bed.on('mousedown', function(){
-    var clone = bed.clone()
+  var bed = draw.image('assets/bed.png', 50,75)
+  bed.move(50,510)
+
+  var couch = draw.image('assets/couch.png', 100,50)
+  couch.move(150, 510)
+
+  var toolBoxFurn = draw.group()
+  toolBoxFurn.add(bed)
+  toolBoxFurn.add(couch)
+
+  toolBoxFurn.each(function(i, children){
+    this.on('click', function(){
+    var clone = this.clone()
     clone.move(0,0)
     clone.draggable({
-      minX: 200
-    , minY: 50
-    , maxX: 600
-    , maxY: 300
+      minX: 0
+    , minY: 0
+    , maxX: 800
+    , maxY: 350
     })
+    // clone.on('click')
+
   })
+  })
+
+
 
 })
