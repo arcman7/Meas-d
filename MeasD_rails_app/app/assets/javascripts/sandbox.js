9fc7b8509b3964ade78abb621d3696de5e4f4063
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2015-06-22 17:18:49 -0700

    add rotate function

--- MeasD_rails_app/app/assets/javascripts/sandbox.js
+++ MeasD_rails_app/app/assets/javascripts/sandbox.js
@@ -1,117 +1,118 @@
 
-$(document).ready(function(){
-  var draw = SVG('drawing').size(800, 600)
+// $(document).ready(function(){
+//   var draw = SVG('drawing').size(800, 600)
 
-  var roomWidth = 400;
-  var roomLength = 250;
-  var line = draw.line(0,500, 800,500).stroke({ width: 1 })
+//   var roomWidth = 400;
+//   var roomLength = 250;
+//   var line = draw.line(0,500, 800,500).stroke({ width: 1 })
 
-  var room = draw.rect(roomWidth,roomLength)
-  room.fill('white')
-  room.stroke({color: 'black', width: 1})
-  room.move(200,50)
+//   var room = draw.rect(roomWidth,roomLength)
+//   room.fill('white')
+//   room.stroke({color: 'black', width: 1})
+//   room.move(200,50)
 
-  $('#room_form').submit(function(e){
-    e.preventDefault();
-    room.width($('#room_width').val())
-    room.height($('#room_length').val())
-  })
+//   $('#room_form').submit(function(e){
+//     e.preventDefault();
+//     room.width($('#room_width').val())
+//     room.height($('#room_length').val())
+//   })
 
-  var bed = draw.image('assets/bed.png', 50,75)
-  bed.move(50,510)
-  bed.attr('name', 'Bed')
-  bed.attr('value', 'bed')
+//   var bed = draw.image('assets/bed.png', 50,75)
+//   bed.move(50,510)
+//   bed.attr('name', 'Bed')
+//   bed.attr('value', 'bed')
 
-  var couch = draw.image('assets/couch.png', 100,50)
-  couch.move(150, 510)
-  couch.attr('name', 'Couch')
-  couch.attr('value', 'couch')
+//   var couch = draw.image('assets/couch.png', 100,50)
+//   couch.move(150, 510)
+//   couch.attr('name', 'Couch')
+//   couch.attr('value', 'couch')
 
-  var toolBoxFurn = draw.group()
-  toolBoxFurn.add(bed)
-  toolBoxFurn.add(couch)
+//   var toolBoxFurn = draw.group()
+//   toolBoxFurn.add(bed)
+//   toolBoxFurn.add(couch)
 
-  var sandboxFurn = draw.group()
-  sandboxFurn.attr('name', 'sandbox')
+//   var sandboxFurn = draw.group()
+//   sandboxFurn.attr('name', 'sandbox')
 
-  toolBoxFurn.each(function(){
-    this.on('click', function(){
-      var clone = this.clone()
-      console.log(clone.id())
-      clone.move(0,0)
-      clone.draggable()
-      sandboxFurn.add(clone)
-    })
-  })
+//   toolBoxFurn.each(function(){
+//     this.on('click', function(){
+//       var clone = this.clone()
+//       console.log(clone.id())
+//       clone.move(0,0)
+//       clone.draggable()
+//       sandboxFurn.add(clone)
+//     })
+//   })
 
-    var element;
-    var selected = false;
-    var a = -1;
-    var selectTop, selectBottom, selectLeft, selectRight;
-    selectTop = draw.line(0,0,0,0).stroke({width: 1}); selectBottom = draw.line(0,0,0,0).stroke({width: 1}); selectLeft = draw.line(0,0,0,0).stroke({width: 1}); selectRight = draw.line(0,0,0,0).stroke({width: 1});
-    var knob = draw.circle(0).stroke({color: "blue", width: 2});
-    var connectKnob = draw.circle(0);
-    var connectLine = draw.line(0,0,0,0);
-    connectKnob.attr({fill: "blue"});
-    knob.attr({ fill: "green"});
-    var padding=3;
+//     var element;
+//     var selected = false;
+//     var a = -1;
+//     var selectTop, selectBottom, selectLeft, selectRight;
+//     selectTop = draw.line(0,0,0,0).stroke({width: 1}); selectBottom = draw.line(0,0,0,0).stroke({width: 1}); selectLeft = draw.line(0,0,0,0).stroke({width: 1}); selectRight = draw.line(0,0,0,0).stroke({width: 1});
+//     var knob = draw.circle(0).stroke({color: "blue", width: 2});
+//     var connectKnob = draw.circle(0);
+//     var connectLine = draw.line(0,0,0,0);
+//     connectKnob.attr({fill: "blue"});
+//     knob.attr({ fill: "green"});
+//     var padding=3;
 
 
 
-    $('svg').on('click', function(){
-      if(a>-1){ a -= 1; }
+//     $('svg').on('click', function(){
+//       if(a>-1){ a -= 1; }
 
-        //console.log("event -",a);
-       //console.log(this.getAttribute('id'));
-      // console.log(element.type);
-      if(a == -1 ){
-        selectTop.plot(0,0,0,0).stroke({ width: 1});
-        selectBottom.plot(0,0,0,0).stroke({ width: 1});
-        selectLeft.plot(0,0,0,0).stroke({ width: 1});
-        selectRight.plot(0,0,0,0).stroke({ width: 1});
-        knob.radius(0);
-        connectKnob.radius(0);
-        connectLine.plot(0,0,0,0).stroke({ width: 1});
-      }
+//         //console.log("event -",a);
+//        //console.log(this.getAttribute('id'));
+//       // console.log(element.type);
+//       if(a == -1 ){
+//         selectTop.plot(0,0,0,0).stroke({ width: 1});
+//         selectBottom.plot(0,0,0,0).stroke({ width: 1});
+//         selectLeft.plot(0,0,0,0).stroke({ width: 1});
+//         selectRight.plot(0,0,0,0).stroke({ width: 1});
+//         knob.radius(0);
+//         connectKnob.radius(0);
+//         connectLine.plot(0,0,0,0).stroke({ width: 1});
+//       }
 
-    })
+//     })
+// //$('svg').on('click', 'g')*******************************
 
-$('svg').on('click', 'g[name="sandbox"] image', function(){
-      if(a==-1){a +=2;}
-      if(a==0){a+=1}
-      element = SVG.get(this.getAttribute('id'))
-      //console.log("event +",a);
+// $('svg').on('click', 'g[name="sandbox"] image', function(){
+//       if(a==-1){a +=2;}
+//       if(a==0){a+=1}
+//       element = SVG.get(this.getAttribute('id'))
+//       //console.log("event +",a);
 
-    if(a == 1 || 0 ){
-       var x = element.x() - padding;
-       var x2 = x + element.width() + padding;
-       var y = element.y() - padding;
-       var y2 = y + element.height() + padding;
-        selectTop.plot(x, y, x2, y ).stroke({ width: 1});
-        selectBottom.plot(x, y2, x2, y2 ).stroke({ width: 1});
-        selectLeft.plot(x, y, x, y2 ).stroke({ width: 1});
-        selectRight.plot(x2, y, x2, y2 ).stroke({ width: 1});
-        knob.radius(4); knob.move(x-2 +(x2-x)/2,y-24)
-        connectKnob.radius(4); connectKnob.move(x-2 +(x2-x)/2,y-4);
-        connectLine.plot(x+2 +(x2-x)/2,y-4,x+2 +(x2-x)/2,y-17).stroke({ width: 1});
-     }
-  })
-    //update form for the furniture
-    $('svg').on('click', 'g[name="sandbox"] image', function(){
-      element = SVG.get(this.getAttribute('id'))
-      $('#furn_name').val(element.attr('name'))
-      $('#furn_width').val(element.width())
-      $('#furn_length').val(element.height())
-      $('#furn_rotation').val(element.transform('rotation'))
-    })
+//     if(a == 1 || 0 ){
+//        var x = element.x() - padding;
+//        var x2 = x + element.width() + padding;
+//        var y = element.y() - padding;
+//        var y2 = y + element.height() + padding;
+//         selectTop.plot(x, y, x2, y ).stroke({ width: 1});
+//         selectBottom.plot(x, y2, x2, y2 ).stroke({ width: 1});
+//         selectLeft.plot(x, y, x, y2 ).stroke({ width: 1});
+//         selectRight.plot(x2, y, x2, y2 ).stroke({ width: 1});
+//         knob.radius(4); knob.move(x-2 +(x2-x)/2,y2+18)
+//         connectKnob.radius(4); connectKnob.move(x-2 +(x2-x)/2,y2);
+//         connectLine.plot(x+2 +(x2-x)/2,y2+4,x+2 +(x2-x)/2,y2+17).stroke({ width: 1});
+//      }
+//   })
+//     //update form for the furniture
+//     $('svg').on('click', 'g[name="sandbox"] image', function(){
+//       element = SVG.get(this.getAttribute('id'))
+//       $('#furn_name').val(element.attr('name'))
+//       $('#furn_width').val(element.width())
+//       $('#furn_length').val(element.height())
+//       $('#furn_rotation').val(element.transform('rotation'))
+//     })
 
-    //update the furniture based on form input
-    $('#furniture_form').on('submit', function(e){
-      e.preventDefault()
-      element.attr('name', $('#furn_name').val())
-      element.width($('#furn_width').val())
-      element.height($('#furn_length').val())
-      element.transform({ rotation: $('#furn_rotation').val() })
-    })
+//     //update the furniture based on form input
+//     $('#furniture_form').on('submit', function(e){
+//       e.preventDefault()
+//       element.attr('name', $('#furn_name').val())
+//       element.width($('#furn_width').val())
+//       element.height($('#furn_length').val())
+//       element.transform({ rotation: $('#furn_rotation').val() })
+//     })
 
-})
+// })
--- MeasD_rails_app/app/assets/javascripts/test.js
+++ MeasD_rails_app/app/assets/javascripts/test.js
@@ -1,3 +1,70 @@
  $(document).on('ready',function(){
+    var test = SVG('test').size(1000, 1000)
+    var rect = test.rect(100,200);
+    rect.move(300,300);
+    var rect2 = test.rect(300,240);
+    rect2.move(600,600);
+    var set = test.set()
+    set.add(rect); set.add(rect2);
+    var vmousedown = false;
+    var mouseX, mouseY;
+    var deltaX_old = 0;
+    var deltaY_old = 0;
+    var deltaX,deltaY;
+
+     var selectTop, selectBottom, selectLeft, selectRight;
+    selectTop = draw.line(0,0,0,0).stroke({width: 1}); selectBottom = draw.line(0,0,0,0).stroke({width: 1}); selectLeft = draw.line(0,0,0,0).stroke({width: 1}); selectRight = draw.line(0,0,0,0).stroke({width: 1});
+    var knob = draw.circle(0).stroke({color: "blue", width: 2});
+    var connectKnob = draw.circle(0);
+    var connectLine = draw.line(0,0,0,0);
+    connectKnob.attr({fill: "blue"});
+    knob.attr({ fill: "green"});
+    var padding=3;
+
+    var element = rect;
+       var x = element.x() - padding;
+       var x2 = x + element.width() + padding;
+       var y = element.y() - padding;
+       var y2 = y + element.height() + padding;
+        selectTop.plot(x, y, x2, y ).stroke({ width: 1});
+        selectBottom.plot(x, y2, x2, y2 ).stroke({ width: 1});
+        selectLeft.plot(x, y, x, y2 ).stroke({ width: 1});
+        selectRight.plot(x2, y, x2, y2 ).stroke({ width: 1});
+        knob.radius(4); knob.move(x-2 +(x2-x)/2,y2+18)
+        connectKnob.radius(4); connectKnob.move(x-2 +(x2-x)/2,y2);
+        connectLine.plot(x+2 +(x2-x)/2,y2+4,x+2 +(x2-x)/2,y2+17).stroke({ width: 1});
+
+    //var elem = $("#SvgjsRect1007");
+        var elem = document.body;
+
+
+   elem.addEventListener("mousedown", function(ev)
+    {
+        vmousedown = true;
+        mouseX = ev.pageX;
+        mouseY = ev.pageY;
+
+    });
+    elem.addEventListener("mousemove", function(ev)
+    {
+        if (vmousedown === false)
+            return;
+        deltaX = ev.pageX - mouseX;
+        deltaY = ev.pageY - mouseY;
+        theta = Math.atan(deltaY/deltaX); degrees = theta*(360/(2*Math.PI))
+        set.rotate(degrees);
+
+    });
+
+    elem.addEventListener("mouseup", function(ev)
+    {
+        theta = Math.atan(deltaY/deltaX); degrees = theta*(360/(2*Math.PI))
+        set.rotate(degrees);
+        vmousedown = false;
+        deltaX_old = 0;
+        deltaY_old = 0;
+
+    });
+
 
  })
